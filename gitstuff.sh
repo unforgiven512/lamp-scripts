998e17c58e21fdb2b6e0b6e33c57f3bee3b09f9a
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2011-05-13 22:47:54 -0400

    started working on git hosting stuff

--- gitstuff.sh
+++ gitstuff.sh
@@ -6,4 +6,19 @@
 
 # USAGE: ./gitstuff.sh [ARG1] [ARG2] [...]
 
+### NOTE:
+### ALL THIS STUFF WILL BE RE-ORGANIZED AND REWRITTEN
+### THIS IS MERELY TEMPORARY -- I AM WRITING THIS
+### SCRIPT AS I SET EVERYTHING UP IN MY VM AS A WAY
+### OF MAKING NOTES FOR MYSELF, TO FIGURE OUT EXACTLY
+### WHAT IS GOING ON. I WOULD NOT RECOMMEND USING THIS
+### IN ITS CURRENT STATE. PLEASE WAIT TILL THIS BECOMES
+### MATURE AND THIS NOTICE IS GONE! THANKS!
+
+source ./options.conf
+source ./constants.conf
+
+# install git, git-doc, gitweb, gitolite
+aptitude -y install git git-doc gitweb gitolite
+
 
